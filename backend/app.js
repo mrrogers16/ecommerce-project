b183378f05b0a102fdf5b4cbe9bc8e66cb29b73e
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-02-26 16:46:20 -0600

    .

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+};
+
+console.log("Database Host:", dbConfig.host);
+
 // app.js
 const express = require('express');
 const app = express();
