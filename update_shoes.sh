1a87f9f1c0b4d70bbbef675e7036a5ddb92e5d87
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-02-28 15:16:12 -0600

    Update update_shoes.sh

--- update_shoes.sh
+++ update_shoes.sh
@@ -2,15 +2,16 @@
 
 echo " Starting S3 Deployment..."
 
-echo "Pulling latest changes from Git..."
-git pull || { echo "Error: Git pull failed"; exit 1; }
+echo "#### Pulling latest changes from Git... ####"
+git pull || { echo "xxx Error: Git pull failed xxx"; exit 1; }
 
 # Path definitions
 STAGING_DIR="/home/ubuntu/ecommerce-project/"
 S3_BUCKET="s3://ecommerce-static-0130"
 
 # Sync files to S3
-echo "Syncing files to S3..."
-aws s3 sync "$STAGING_DIR" "$S3_BUCKET" --delete || { echo "Error: S3 sync failed"; exit 1; }
+# As of now, the entire ecommerce-project directory is copied to the S3 bucket. May change to individual folders in the future.
+echo "#### Syncing files to S3... ####"
+aws s3 sync "$STAGING_DIR" "$S3_BUCKET" --delete || { echo "xxx Error: S3 sync failed xxx"; exit 1; }
 
-echo "Deployment to S3 complete"
+echo "#### Deployment to S3 complete ####"
