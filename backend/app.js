f708ff39a2bfd5c7a6efcd87b6ac66a1465effbf
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-02-28 19:15:02 -0600

    working on user auth/login
    Start back at note source. Stopped on dbConfig.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,47 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-};
+// const dbConfig = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// };
 
-console.log("Database Host:", dbConfig.host);
+// console.log("Database Host:", dbConfig.host);
 
-// app.js
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+// // app.js
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
 const homePage = "http://ecommerce-static-0130.s3-website.us-east-2.amazonaws.com"
 
 app.get('/', (req, res) => {
     res.redirect(homePage);
 });
 
-app.listen(port, () => {
-    console.log(`Backend running on http://localhost:${port}`);
-    console.log(`Frontend available at : ${homePage}`);
-});
+// app.listen(port, () => {
+//     console.log(`Backend running on http://localhost:${port}`);
+//     console.log(`Frontend available at : ${homePage}`);
+// });
 
-//test
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middlewares/authMiddleware');
+const userModel = require('./models/userModel'); // To be used in protected route
+
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+// Start server
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+});
---
+++ backend/config/dbConfig.js
@@ -0,0 +1,18 @@
+// config/dbConfig.js
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+// Create a connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Export the pool, so other files can use it
+module.exports = pool;
