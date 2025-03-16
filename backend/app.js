203ae77dddddf38469f7aa9598a62f054deef3c2
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-03-16 14:17:48 -0500

    debug

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
+require('dotenv').config();
+console.log('DEBUG ENV:', {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT
+});
+
+const { Pool } = require('pg');
+
 const express = require('express');
 const app = express();
 
