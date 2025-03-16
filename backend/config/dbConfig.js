75f908dc1de6c0ddfbdb07c1d5b89c57787ec118
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-03-16 14:33:13 -0500

    Update dbConfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
