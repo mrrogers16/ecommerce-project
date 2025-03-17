e513a3d44dc847b1dd09356221406b60ea83b24f
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-03-17 14:32:36 -0500

    added api/shoes/:id route

--- backend/routes/shoesRoutes.js
+++ backend/routes/shoesRoutes.js
@@ -11,18 +11,27 @@ router.get('/shoes', async (req, res) => {
         res.json(result.rows);
     } catch (error) {
         console.error('Error fetching shoes:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error - /shoes' });
     }
 });
 
 // GET /api/shoes/:id - Return single shoe by ID
-router.get('/shoes', async (req, res) => {
+router.get('/shoes/:id', async (req, res) => {
+
+    const { id } = req.params; // Grab the id from the URL
+
     try {
-        const result = await pool.query('SELECT * FROM shoes');
-        res.json(result.rows);
+        const result = await pool.query('SELECT * FROM shoes WHERE id = $1', [id]);
+
+        // If none found return 404
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Shoe not found' });
+        }
+
+        res.json(result.rows[0]);
     } catch (error) {
         console.error('Error fetching shoes:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error - /shoes/:id' });
     }
 });
 
