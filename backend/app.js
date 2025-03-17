3d14d194c0bf9b06c9ccef35761bac6b5cf7e057
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-03-17 16:54:10 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ console.log('DEBUG ENV:', {
     DB_USER: process.env.DB_USER,
     DB_PASS: process.env.DB_PASS,
     DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT
+    DB_PORT: process.env.DB_PORT,
+    JWT_SECRET: process.env.JWT_SECRET
 });
 
 //const { Pool } = require('pg');
