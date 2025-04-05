d1608363f047163f90085e9e3b6c6101a23e63d1
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-04-05 14:20:39 -0500

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     window.location.href = "/admin.html";
                 }
                 else {
-                    window.location.href = "/index.html";
+                    window.location.href = "../index.html";
                 }
             }
             else {
